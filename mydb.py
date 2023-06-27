7a31f60f03b1fe53d02dd23b8a82f8cbb1145419
Klemen Stanic <klemenstanic@gmail.com>
2023-06-27 23:50:32 +0200

    Initial commit

---
+++ mydb.py
@@ -0,0 +1,18 @@
+
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'Kle3men123'
+
+	)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE dcrmdatabase")
+
+print("All Done!")
